ebbb66a2cda945b84477445ca410280fb51b89ea
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-11-29 21:42:01 -0600

    starter code

---
+++ truffle/truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "YOUR RPC ENDPOINT HERE",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 4712388
+    }
+  }
+};
